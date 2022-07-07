a50e76abc7dbc9435b7e1b5426c31b90e65d03cb
seoyoung0714 <seoyoung0714@naver.com>
2022-07-07 19:49:48 +0900

    Chapter

---
+++ Chapter2/src/binary/ByteVariable.java
@@ -0,0 +1,17 @@
+package binary;
+
+public class ByteVariable {
+
+	public static void main(String[] args) {
+		
+		byte bData1 = -128;
+		
+		System.out.println(bData1);
+		
+		byte bData2 = 127;
+		
+		System.out.println(bData2);
+
+	}
+
+}
---
+++ Chapter2/src/binary/DoubleEx.java
@@ -0,0 +1,21 @@
+package binary;
+
+public class DoubleEx {
+
+	public static void main(String[] args) {
+		
+		double dNum = 3.14;
+		float fNum = 3.14F;
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
+		
+		var vNum = 3.14;
+		var num = 3;
+		
+		System.out.println(vNum);
+		System.out.println(num);
+
+	}
+
+}
